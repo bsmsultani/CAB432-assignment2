93ac966c0966abcbc95094e02ce7d491db4a4fd4
Millah <bsmsultani@gmail.com>
2023-10-08 11:32:57 +0000

    more changes

--- client/src/components/VideoDropBox.js
+++ client/src/components/VideoDropBox.js
@@ -1,39 +1,47 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import './videoDropBox.css';
 
 const VideoDropBox = () => {
-  // Create a ref to store the file input element
-  // a ref is a way to access a DOM element in React
-  // the video file is stored in the ref
-
   const videoFile = useRef(null);
   const SERVER = localStorage.getItem('server');
   const UPLOAD_URL = `${SERVER}/api/video/upload`;
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(null);
 
-  // Function to handle form submission
-  // NEED TO ADD ERROR HANDLING SUCH AS IF NO FILE IS SELECTED
-  // IF THE SERVER IS DOWN, ETC.
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    // Create a FormData object to store the selected video file
-    const formData = new FormData();
-    formData.append('video', videoFile.current.files[0]);
 
-    fetch(UPLOAD_URL, {
-      method: 'POST',
-      body: formData
-    })
-    .then(res => res.json())
-    .then(data => {
-      alert(data);
-    })
-    .catch(err => {
-      console.log(err);
-      alert(`Error uploading video, ${err}`);
-    });
-  }
+    if (!videoFile.current.files[0]) {
+      alert("Please select a video file.");
+      return;
+    }
+
+    setUploading(true);
+    setUploadError(null);
+
+    try {
+      const formData = new FormData();
+      formData.append('video', videoFile.current.files[0]);
+
+      const response = await fetch(UPLOAD_URL, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        alert(data.message);
+      } else {
+        const errorData = await response.json();
+        setUploadError(errorData.error || "Unknown error occurred.");
+      }
+    } catch (err) {
+      console.error(err);
+      setUploadError(`Error uploading video: ${err.message}`);
+    } finally {
+      setUploading(false);
+    }
+  };
 
   return (
     <div className="video-drop-box">
@@ -44,12 +52,11 @@ const VideoDropBox = () => {
           accept="video/*" 
           ref={videoFile}
         />
-        <input type="submit" value="Upload" />
+        <input type="submit" value="Upload" disabled={uploading} />
+        {uploadError && <p className="error-message">{uploadError}</p>}
       </form>
     </div>
   );
-}
+};
 
 export default VideoDropBox;
-
-//hello
--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from 'express';
 
 const app = express();
- 
+const port = process.env.PORT || 3001;
 
+// Parse JSON and URL-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Handle the video upload route
 app.post('/api/video/upload', (req, res) => {
-    // get the video which is sent 
-    res.json({success: true});
+    console.log('uploading video');
+    
+  // Check if there's a video file in the request body
+  if (!req.body.video) {
+    return res.status(400).json({ error: 'No video data provided.' });
+  }
+
+  // Access the video data from the request body
+  const videoData = req.body.video;
+
+  console.log(videoData);
+
+  // Now you can process the videoData as needed, e.g., you can save it to a database or perform other operations.
+
+  // Send a response indicating success
+  res.status(200).json({ message: 'Video data received and processed successfully.' });
 });
 
-
-
-
-app.listen(3001, () => console.log('Server ready, visit http://localhost:3001'))
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
