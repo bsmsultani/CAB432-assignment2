f54fea551ca52400551b9c91c9b62cff1979ed84
Ubuntu <ubuntu@ip-172-31-120-41.ap-southeast-2.compute.internal>
2023-10-31 12:09:26 +0000

    changes

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -2,27 +2,25 @@ import path from 'path';
 import RedisUtils from '../utils/redisUtils.js';
 import S3Utils from '../utils/s3Utils.js';
 
-const bucketName = "cab432group100";
-
 async function upload(req, res, next) {
-    const video_hash = req.body.video_hash;
+    if (!req.body.video_hash) {
+        return res.status(400).send({ message: "Missing video_hash" });
+    }
+
     
+    const video_hash = req.body.video_hash;
+
     console.log('video_hash', video_hash);
 
     const redis = new RedisUtils(req.redisClient, video_hash);
-    if (await redis.isVideoProcessed() == false) {
-
-        const s3 = new S3Utils(req.AWS);
-        const results = await s3.listBucketContents();
-        const url = await s3.getSignedUrl(video_hash);
-        console.log('url', url);
-        console.log('results', results);
-        
+    if (await redis.isVideoProcessed()) {
         return res.status(200).send({ message: "Video already processed" });
     }
 
-    res.send({ message: "Video uploaded successfully" });
-
+    const s3 = new S3Utils(req.AWS);
+    const url = await s3.getSignedUrl(video_hash);
+    
+    res.status(200).send({ s3url: url});
 }
 
 export default upload;
