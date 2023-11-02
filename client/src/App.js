af6124b7f10e3f08545249c051615c82b0efc45a
Ubuntu <ubuntu@ip-172-31-120-41.ap-southeast-2.compute.internal>
2023-11-02 08:39:28 +0000

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   // const server = process.env.REACT_APP_SERVER_URL;
   // get the server url from the environment variable during docker deployment
 
-  const server = 'http://group109-loadbalancer-1764708288.ap-southeast-2.elb.amazonaws.com';
+  const server = 'http://group109lb-1372671419.ap-southeast-2.elb.amazonaws.com';
   localStorage.setItem('server', server);
 
 
