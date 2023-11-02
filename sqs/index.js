993242e0076ab5c7d03592aab885edb201e0fb1a
Ubuntu <ubuntu@ip-172-31-120-41.ap-southeast-2.compute.internal>
2023-11-02 08:53:28 +0000

    added health checks for sqs

--- sqs/index.js
+++ sqs/index.js
@@ -1,10 +1,26 @@
 import SqsUtils from "./utils/sqsUtils.js";
 import AWS from "aws-sdk";
+import express from "express";
 
-AWS.config.update({
-    region: "ap-southeast-2"
+
+const app = express();
+
+try {
+
+    AWS.config.update({
+        region: "ap-southeast-2"
+    });
+
+    const sqsUtils = new SqsUtils(AWS);
+    sqsUtils.startPolling();
+} catch (e) {
+    console.log(e);
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
 });
 
-
-const sqsUtils = new SqsUtils(AWS);
-sqsUtils.startPolling();
+app.listen(3000, () => {
+    console.log("SQS app listening on port 3000!");
+});
